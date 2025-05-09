1702a97ea95a9683f7206339b62b5fe2d52c4f4e
Fadimecrk <fadime_986@outlook.com>
2025-05-09 20:21:00 +0200

    Update HotellContext.cs

--- HotellContext.cs
+++ HotellContext.cs
@@ -7,6 +7,21 @@ using System.Threading.Tasks;
 using HotelApp.Models;
 using Microsoft.EntityFrameworkCore;
 
+
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    modelBuilder.Entity<Kund>().HasData(
+        new Kund { Id = 1, Namn = "Anna Karlsson", Epost = "anna@example.com" },
+        new Kund { Id = 2, Namn = "Olle Olsson", Epost = "olle@example.com" }
+    );
+
+    modelBuilder.Entity<Rum>().HasData(
+        new Rum { Id = 1, Rumsnummer = "101", Typ = "Enkel", PrisPerNatt = 500 },
+        new Rum { Id = 2, Rumsnummer = "102", Typ = "Dubbel", PrisPerNatt = 800 }
+    );
+}
+
+
 namespace HotelApp.Data;
 
 public class HotellContext : DbContext
